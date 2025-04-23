c70b2aea17ec01775ca9823db96deb8709151626
tomoe.shimizu <shimizu.tomoe.ts@gmail.com>
2025-04-23 10:23:41 +0900

    Fix: set branch name to 'main'

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, "https://github.com/coding-shimichan/dpro_assignment_ec2_deploy.g
 set :bundle_without, %w{test}.join(':')
 
 # Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+ask :branch, "main"
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, "/var/www/my_app_name"
